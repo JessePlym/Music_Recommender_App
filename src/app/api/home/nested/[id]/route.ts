2417c0b300bdefe7ee81571a22eddb4468c8efa4
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-05 11:02:04 +0300

    unused file deleted

--- src/app/api/home/nested/[id]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1)
-  return NextResponse.json({ "id": id})
-}
