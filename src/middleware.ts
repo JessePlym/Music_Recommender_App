bc3e6925b9f47fdb52f3102bd688d80fb1c7083e
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-05 19:34:43 +0300

    cors settings added

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,17 @@ export function Middleware(request: NextRequest) {
       }
     })
   }
+  const res = NextResponse.next()
+
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', allowedOrigins[0])
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
 }
 // export const config = {
 //   matcher: ["/", "/preferences", "/api"]
