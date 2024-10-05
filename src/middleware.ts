ed2d4a4e41249407315e4a5c8e83efbee112c1ba
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-05 19:26:43 +0300

    handled cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
 export { default } from "next-auth/middleware"
 
+const allowedOrigins = process.env.NODE_ENV === "production"
+  ? ["https://music-recommender-app.vercel.app"]
+  : ["http://localhost:3000"]
+export function Middleware(request: NextRequest) {
+
+  const origin = request.headers.get("origin")
+
+  if (origin && !allowedOrigins.includes(origin) || !origin) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad Request",
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    })
+  }
+}
 // export const config = {
-//   matcher: ["/", "/preferences", "/api/songs/recent"]
+//   matcher: ["/", "/preferences", "/api"]
 // }
