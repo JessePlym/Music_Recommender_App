e4259caff97280c3f6f29ee44bfef70ec44940fa
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-05 19:37:52 +0300

    hostname changed

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 //export const DATA_SOURCE_URL: string = "http://localhost:3500"
-export const HOSTNAME: string = "http://localhost:3000"
+export const HOSTNAME: string = "https://music-recommender-app.vercel.app"
