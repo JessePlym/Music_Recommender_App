357f3ef3a82b0d9ba37f568e67dabdef40584d17
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-06 18:33:54 +0300

    header styles changed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,12 +4,10 @@ import Link from "next/link"
 import { useState } from "react"
 import { FaWrench, FaSignOutAlt } from "react-icons/fa"
 import useWindowSize from "../hooks/useWindowSize"
-import { useSession } from "next-auth/react"
 
 export default function Header() {
   const [searchInput, setSearchInput] = useState("")
   const { width } = useWindowSize()
-  const { data: session } = useSession()
 
   return (
     <header className="relative bg-teal-700 text-white top-0 z-20 flex flex-col sm:flex-row justify-between items-center">
@@ -20,7 +18,7 @@ export default function Header() {
         : <h1></h1>
       }
       <nav className="mx-10 sm:w-1/2 w-5/6 flex items-center h-full justify-between">
-        <form className="lg:w-1/2 w-full" onSubmit={e => e.preventDefault()}>
+        <form className="lg:w-1/2 w-full hidden md:block" onSubmit={e => e.preventDefault()}>
           <input
             className="text-2xl h-full p-2 rounded-lg text-black w-full focus:outline-none focus:border-1 border border-teal-500"
             type="text"
@@ -36,17 +34,20 @@ export default function Header() {
             <Link href="/preferences">
               <FaWrench />
             </Link>
-            { session !== null && 
-              <Link href="/api/auth/signout">
-                <FaSignOutAlt />
-              </Link>
-            }
-          </div>
-          : 
-          session !== null && 
             <Link href="/api/auth/signout">
               <FaSignOutAlt />
             </Link>
+          </div>
+          : 
+          <div className="flex flex-row w-full justify-between py-4">
+            <Link className="hover:text-slate-300" href="/">Home</Link>
+            <Link href="/preferences">
+              <FaWrench />
+            </Link>
+            <Link href="/api/auth/signout">
+              <FaSignOutAlt />
+            </Link>
+          </div>
           }
       </nav>
     </header>
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 type Props = {
-  error: Error & { digest?: string },
-  reset: () => void
+  error: Error & { digest?: string }
 }
 
-export default function Error({ error, reset }: Props) {
+export default function Error({ error }: Props) {
+  const router = useRouter()
+
   useEffect(() => {
     console.log(error)
   }, [error])
@@ -15,7 +17,7 @@ export default function Error({ error, reset }: Props) {
     <>
       <h2>Something went wrong!</h2>
       <button
-        onClick={() => reset()}
+        onClick={() => router.push("/api/auth/signin")}
       >
         Try Again
       </button>
