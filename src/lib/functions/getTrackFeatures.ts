8d59beefdbd0ddbe3fbde098635a5d6ffdcd271a
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-10-07 10:28:23 +0300

    audio fetching endpoint updated

--- src/lib/functions/getTrackFeatures.ts
+++ src/lib/functions/getTrackFeatures.ts
@@ -1,41 +1,46 @@
 const SPOTIFY_DATA_SOURCE_URL = "https://api.spotify.com/v1"
 
+type AudioFeature = {
+  id: string,
+  acousticness: number,
+  danceability: number,
+  instrumentalness: number,
+  key: number,
+  mode: number,
+  tempo: number
+}
+
 export async function getTrackFeatures(tracks: Track[], accessToken: string) {
   if (tracks.length === 0) return
 
-  for (const track of tracks) {
-    try {
-      const response = await fetch(`${SPOTIFY_DATA_SOURCE_URL}/audio-features/${track.id}`, {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${accessToken}`
-        }
-      })
-      if (response.ok) {
-        const featureData = await response.json()
-        track.features.acousticness = featureData.acousticness
-        track.features.danceability = featureData.danceability
-        track.features.instrumentalness = featureData.instrumentalness
-        track.features.key = featureData.key
-        track.features.mode = featureData.mode
-        track.features.tempo = featureData.tempo
-        await timeout(10)
-      } else if (response.status === 429) {
-        console.log("Too many requests", response)
-        break
-      } else {
-        console.log("Could not add features")
-        break
-      }
-      
-    } catch (err) {
-      console.log(err)
-      break
-    }
-  }
-  
-}
+  const trackIds = tracks.map(track => track.id)
+  const trackIdsString = trackIds.join(",")
 
-function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  try {
+    const response = await fetch(`${SPOTIFY_DATA_SOURCE_URL}/audio-features?ids=${trackIdsString}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${accessToken}`
+      }
+    })
+    if (response.ok) {
+      const data = await response.json()
+      const audioFeatures: AudioFeature[] = data.audio_features
+      for (const feature of audioFeatures) {
+        for (const track of tracks) {
+          if (track.id === feature.id) {
+            track.features.acousticness = feature.acousticness
+            track.features.danceability = feature.danceability
+            track.features.instrumentalness = feature.instrumentalness
+            track.features.key = feature.key
+            track.features.mode = feature.mode
+            track.features.tempo = feature.tempo
+          }
+        }
+      }
+    } 
+  } catch (err) {
+    console.log(err)
+    
+  } 
 }
--- src/lib/requests/getSongData.ts
+++ src/lib/requests/getSongData.ts
@@ -1,50 +1,74 @@
 import { HOSTNAME } from "../../../constants"
 
 export async function getSongData(accessToken: string, artistIds: string[], userId: string) {
-  let songData: Track[]
+  let songData: Track[] = []
+  let dataReceivedFromMongo: boolean = false
 
   if (accessToken) {
-    try {
-      const response = await fetch(`${HOSTNAME}/api/songs/song-data?id=${userId}`, {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${accessToken}`
-        },
-        body: JSON.stringify(artistIds)
-      })
-      if (response.ok) {
-        const data = await response.json()
-        songData = data.tracks
-        if (data.dataFromMongo) {
-          return songData
+
+    /**
+     *  fetch songs in batches of 3 artist, prevents 504 in vercel 
+     */
+
+    for (let i = 0; i < artistIds.length; i = i + 3) {
+
+      const partialArtistIds = artistIds.slice(i, i + 3)
+      let partialSongData: Track[] = []
+
+      try {
+        const response = await fetch(`${HOSTNAME}/api/songs/song-data?id=${userId}`, {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${accessToken}`
+          },
+          body: JSON.stringify(partialArtistIds)
+        })
+        if (response.ok) {
+          const data = await response.json()
+          if (data.dataFromMongo) {
+            dataReceivedFromMongo = true
+            songData = data.tracks
+            break
+          }
+          partialSongData = data.tracks
+          songData = [...songData, ...partialSongData]
+
+        } else {
+          console.log(response.status)
         }
-      } else {
-        console.log(response.status)
-        return null
+      } catch (err) {
+        console.log("Error while fetching data")
       }
+    }
+
+    if (dataReceivedFromMongo) {
+      return songData
+    }
 
       /**
-       * Add features in batches of 50 so that vercel doesn't return 504
+       * Add features in batches of 100
        */
 
-      let songsToReturn: Track[] = []
+    let songsToReturn: Track[] = []
 
-      for (let i = 0; i < songData.length; i = i + 50) {
-        const partialSongData = songData.slice(i, i + 50)
-        const songDataWithFeatures = await addAudioFeatures(accessToken, partialSongData, false, userId)
-        songsToReturn = [...songsToReturn, ...songDataWithFeatures]
-      }
+    for (let i = 0; i < songData.length; i = i + 100) {
+      console.log("fetching audio-features")
+      const partialSongData = songData.slice(i, i + 100)
+      const songDataWithFeatures = await addAudioFeatures(accessToken, partialSongData, false, userId)
+      console.log(songDataWithFeatures)
+      songsToReturn = [...songsToReturn, ...songDataWithFeatures]
+    }
 
       /**
        * Make one more request to save all songs to db
        */
 
+    if (songsToReturn.length !== 0) {
       await addAudioFeatures(accessToken, songsToReturn, true, userId)
-
-      return songsToReturn
-    } catch (err) {
-      console.log("Error while fetching data")
     }
+
+    return songsToReturn
+    
   } else {
     console.log("No token")
   }
